ab3c87b34c898b35ee9a873d0665f4f607bdb634
Vladislav Pyatkov <vldpyatkov@gmail.com>
2025-06-30 15:56:32 +0300

    Adopted benchmark to the server infrastructure.

--- src/main/java/org/gridgain/benchmark/BaseBenchmark.java
+++ src/main/java/org/gridgain/benchmark/BaseBenchmark.java
@@ -1,5 +1,6 @@
 package org.gridgain.benchmark;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.ThreadLocalRandom;
@@ -7,15 +8,16 @@ import java.util.concurrent.ThreadLocalRandom;
 public class BaseBenchmark {
 
     protected String licenseUrl() {
-        return "C:\\ignite2\\gridgain\\ggprivate\\gridgain-license.xml";
+//        return "C:\\ignite2\\gridgain\\ggprivate\\gridgain-license.xml";
+        return "/root/pvd/gg8Benchmark/gridgain-license.xml";
     }
 
     protected long maxMemorySize() {
-        return 5368709120L;
+        return 21474836480L; // 20 GB
     }
 
     protected Path workDir() throws Exception {
-        return Paths.get("D:", "Ignite2tmpDirPrefix" + ThreadLocalRandom.current().nextInt());
-//        return Files.createTempDirectory("Ignite2tmpDirPrefix").toFile().toPath();
+//        return Paths.get("D:", "Ignite2tmpDirPrefix" + ThreadLocalRandom.current().nextInt());
+        return Files.createTempDirectory("Ignite2tmpDirPrefix").toFile().toPath();
     }
 }
--- src/main/java/org/gridgain/benchmark/ClientKvBenchmark.java
+++ src/main/java/org/gridgain/benchmark/ClientKvBenchmark.java
@@ -45,7 +45,7 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 
 @State(Scope.Benchmark)
 @Fork(1)
-@Threads(16)
+@Threads(32)
 @Warmup(iterations = 10, time = 2)
 @Measurement(iterations = 20, time = 2)
 @BenchmarkMode(Mode.Throughput)
